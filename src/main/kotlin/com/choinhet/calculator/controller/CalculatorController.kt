890a8f42cac89665e5a56dcff82b3ec85f3cb74e
Rafael Choinhet <rafael.choinhet@looqbox.com>
2023-07-02 20:52:05 -0300

    Optimize import

--- src/main/kotlin/com/choinhet/calculator/controller/CalculatorController.kt
+++ src/main/kotlin/com/choinhet/calculator/controller/CalculatorController.kt
@@ -1,7 +1,6 @@
 package com.choinhet.calculator.controller
 
 import com.choinhet.calculator.service.CalculatorService
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody

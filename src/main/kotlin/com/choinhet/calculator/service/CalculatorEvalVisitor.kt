27d0dfde13e5e2d7abc8918ec5d338ebfe4f89cd
Rafael Choinhet <rafael.choinhet@looqbox.com>
2023-07-03 08:57:52 -0300

    Remove redundant code

--- src/main/kotlin/com/choinhet/calculator/service/CalculatorEvalVisitor.kt
+++ src/main/kotlin/com/choinhet/calculator/service/CalculatorEvalVisitor.kt
@@ -2,12 +2,6 @@ import com.choinhet.calculator.CalculatorBaseVisitor
 import com.choinhet.calculator.CalculatorParser
 
 class CalculatorEvalVisitor : CalculatorBaseVisitor<Float>() {
-    private val operationVocabulary = mapOf(
-        "*" to listOf("*", "x", "X", "multiplied by", "times", "multiplied"),
-        "/" to listOf("/", "÷", "divided by", "over"),
-        "+" to listOf("+", "plus", "added to", "add", "sum"),
-        "-" to listOf("-", "minus", "subtracted from", "subtract", "sub")
-    )
 
     private val operatorMap = mapOf(
         "*" to { left: Float, right: Float -> left.times(right) },
@@ -23,10 +17,10 @@ class CalculatorEvalVisitor : CalculatorBaseVisitor<Float>() {
     override fun visitExpression(ctx: CalculatorParser.ExpressionContext): Float {
         return when {
             ctx.NUMBER() != null -> ctx.NUMBER().text.toFloat()
-            ctx.mult() != null -> evaluateBinaryExpression(ctx, ctx.mult().text)
-            ctx.div() != null -> evaluateBinaryExpression(ctx, ctx.div().text)
-            ctx.sum() != null -> evaluateBinaryExpression(ctx, ctx.sum().text)
-            ctx.sub() != null -> evaluateBinaryExpression(ctx, ctx.sub().text)
+            ctx.mult() != null -> evaluateBinaryExpression(ctx, "*")
+            ctx.div() != null -> evaluateBinaryExpression(ctx, "/")
+            ctx.sum() != null -> evaluateBinaryExpression(ctx, "+")
+            ctx.sub() != null -> evaluateBinaryExpression(ctx, "-")
             ctx.parenthesisExpression() != null -> visit(ctx.parenthesisExpression().expression())
             else -> throw IllegalArgumentException("Invalid expression")
         }
@@ -35,7 +29,7 @@ class CalculatorEvalVisitor : CalculatorBaseVisitor<Float>() {
     private fun evaluateBinaryExpression(ctx: CalculatorParser.ExpressionContext, operator: String): Float {
         val left = visit(ctx.expression(0))
         val right = visit(ctx.expression(1))
-        val operation = operationVocabulary.entries.find { it.value.contains(operator) }?.key.let { operatorMap[it] }
+        val operation = operatorMap[operator]
             ?: throw IllegalArgumentException("Invalid operator: $operator")
         return operation(left, right)
     }

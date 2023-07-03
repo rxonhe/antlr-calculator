9f9e9646a506d894c3561c8f045e02c03e0fa096
Rafael Choinhet <rafael.choinhet@looqbox.com>
2023-07-03 14:00:50 -0300

    Add cross origin

--- src/main/kotlin/com/choinhet/calculator/controller/CalculatorController.kt
+++ src/main/kotlin/com/choinhet/calculator/controller/CalculatorController.kt
@@ -2,12 +2,10 @@ package com.choinhet.calculator.controller
 
 import com.choinhet.calculator.service.CalculatorService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:3000"])
 @RequestMapping("/calculator")
 class CalculatorController(private val calculatorService: CalculatorService) {
     @PostMapping("/evaluate")
